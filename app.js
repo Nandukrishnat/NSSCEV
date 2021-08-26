0a38ac99bd30a7adc96148bcf337106fc66bf5dc
Nandu Krishna <51917729+Nandukrishnat@users.noreply.github.com>
2021-08-26 12:04:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var sk = document.getElementById('scroll');
 
-ni.addEventListener('mousewheel', function(e){
+sk.addEventListener('mousewheel', function(e){
     e.preventDefault();
    sk.scrollLeft+=e.wheelDelta*0.4;
 })

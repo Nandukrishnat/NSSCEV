1000a1a20ae5988175769fd7030dc9d39b16a73d
Nandu Krishna <nandu@archlinux.localdomain>
2021-08-27 17:31:29 +0530

    Footer And Images added

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var sk = document.getElementById('scroll');
 
-sk.addEventListener('mousewheel', function(e){
+sk.addEventListener('mousewheel', function (e) {
     e.preventDefault();
-   sk.scrollLeft+=e.wheelDelta*0.4;
+    sk.scrollLeft += e.wheelDelta * 0.4;
+})
+
+window.addEventListener('scroll', function () {
+    let header = document.querySelector('header');
+
+    header.classList.toggle('scrolling-active', window.scrollY > 0);
 })
